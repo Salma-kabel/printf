24719f6b4856f59beacc2a5c2a83269cbd8f786f
SamarMI <samar.mohamed.5523@gmail.com>
2023-05-29 15:26:39 +0300

    push edit

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <stdio.h>
+
+/* printf.c */
+int _printf(const char *format, ...);
+
+
+#endif
