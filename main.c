dacd6e53bdb901095b977ebd9b920c73e53a8a07
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-30 11:09:42 -0700

    answer

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(void)
 	ui = (unsigned int)INT_MAX + 1024;
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
-
+	_printf("%S\n", "Best\nSchool");
 	_printf("Length my fn:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 	 _printf("Negative:[%d]\n", -762534);
