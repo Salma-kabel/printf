19d104342a2162c884800b21c766b6f9f8ce99cb
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-31 08:09:54 -0700

    answer

--- main.c
+++ main.c
@@ -7,9 +7,10 @@
  */
 int main(void)
 {
-	int len = _printf("%K\n");	
+	int len = _printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+	int len2 = printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');	
 	
-	printf("%d , \n",len);
+	printf("%d , %d",len,len2);
 
 	return(0);
 }
--- printf.c
+++ printf.c
@@ -10,23 +10,23 @@ int _printf(const char *format, ...)
 {
 	va_list list;
 	int i, length = 0;
-
-	va_start(list, format);
+	
 	if (format == NULL){
 		return (0);
 	}
 	else if (strlen(format) == 1 && format[0] == '%')
 	{
 		_putchar('\n');
-		return (0);
+		return (1);
 	}
+	va_start(list, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == 's' && format[i - 1] == '%')
 			length += s_specifier(va_arg(list, char *));
 		else if (format[i] == 'c' && format[i - 1] == '%')
 			length += c_specifier(va_arg(list, int));
-		else if (format[i] == '%' && format[i - 1] == '%')
+		else if (format[i] == '%' && format[i - 1] == '%' && format[i - 2] != '%')
 			length += percent_specifier(format[i]);
 		else if ((format[i] == 'd' || format[i] == 'i')  && format[i - 1] == '%')
 			length += d_and_i_specifier(va_arg(list, int));
