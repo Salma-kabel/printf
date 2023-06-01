323a50897195a4a8cb2daf4b59ca36c57352be49
SamarMI <samar.mohamed.5523@gmail.com>
2023-06-01 13:34:03 +0300

    edit readme.md v4

--- main.c
+++ main.c
@@ -33,8 +33,8 @@ int main(void)
 {
 	int len, len2;
 
-	len = _printf("%d", 0);
-	len2 = printf("%d", 0);
+	len = _printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	len2 = printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
 	fflush(stdout);
 	if (len != len2)
 	{
