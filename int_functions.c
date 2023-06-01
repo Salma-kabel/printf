4367dc30a2048610c02bb86cd0874b9d63230237
SamarMI <samar.mohamed.5523@gmail.com>
2023-06-01 13:26:46 +0300

    modife i and d specifier

--- int_functions.c
+++ int_functions.c
@@ -7,24 +7,6 @@
  */
 int d_and_i_specifier(int n)
 {
-	/*char str1[100];
-	int length = 0;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		tostring(str1, -n);
-		write(1, str1, strlen(str1));
-		length = 1;
-	}
-	else
-	{
-		tostring(str1, n);
-		write(1, str1, strlen(str1));
-	}
-	length += strlen(str1);
-	return (length);*/
-
 	char *ptr;
 
 	ptr = itoa(n, 10);
